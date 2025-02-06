7f7a18edb84eb07c0589a8dcbfa28f66d8e09fe8
ygiros <yann.giros@soprasteria.com>
2025-02-06 14:44:41 +0100

    Init gradle multi-modules project

---
+++ reactive-delegate/src/main/java/com/example/micrometer/tracing/reactor/delegate/ReactiveDelegateApplication.java
@@ -0,0 +1,11 @@
+package com.example.micrometer.tracing.reactor.delegate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveDelegateApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveDelegateApplication.class, args);
+	}
+}
--- reactive-front/src/main/java/com/example/micrometer/tracing/reactor/front/ReactiveFrontApplication.java
+++ reactive-front/src/main/java/com/example/micrometer/tracing/reactor/front/ReactiveFrontApplication.java
@@ -1,13 +1,11 @@
-package com.example.micrometer.tracing.reactor;
+package com.example.micrometer.tracing.reactor.front;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicrometerTracingReactorApplication {
-
+public class ReactiveFrontApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(MicrometerTracingReactorApplication.class, args);
+		SpringApplication.run(ReactiveFrontApplication.class, args);
 	}
-
 }
