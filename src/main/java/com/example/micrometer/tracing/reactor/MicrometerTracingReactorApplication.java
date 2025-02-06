0ea34c7e7d402e89b53f33549bcfe2b5956c5fe5
ygiros <yann.giros@soprasteria.com>
2025-02-06 14:14:51 +0100

    Init project (spring initialzr)

---
+++ src/main/java/com/example/micrometer/tracing/reactor/MicrometerTracingReactorApplication.java
@@ -0,0 +1,13 @@
+package com.example.micrometer.tracing.reactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrometerTracingReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrometerTracingReactorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/micrometer/tracing/reactor/MicrometerTracingReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.micrometer.tracing.reactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrometerTracingReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
