ca3b9d82bfdc1c902e527e9567971cb2fce7bd06
ygiros <yann.giros@soprasteria.com>
2025-03-05 11:17:03 +0100

    Switch from DefaultTracingObservationHandler to ReactorNettyTracingObservationHandler in ObservationRegistryCustomizer

--- reactive-front/src/main/java/com/example/micrometer/tracing/reactor/front/config/MyObservationRegistryCustomizer.java
+++ reactive-front/src/main/java/com/example/micrometer/tracing/reactor/front/config/MyObservationRegistryCustomizer.java
@@ -1,11 +1,11 @@
 package com.example.micrometer.tracing.reactor.front.config;
 
 import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.tracing.handler.DefaultTracingObservationHandler;
 import io.micrometer.tracing.otel.bridge.OtelTracer;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.actuate.autoconfigure.observation.ObservationRegistryCustomizer;
 import org.springframework.stereotype.Component;
+import reactor.netty.observability.ReactorNettyTracingObservationHandler;
 
 @Component
 public class MyObservationRegistryCustomizer implements ObservationRegistryCustomizer<ObservationRegistry> {
@@ -18,6 +18,6 @@ public class MyObservationRegistryCustomizer implements ObservationRegistryCusto
 
 	@Override
 	public void customize(ObservationRegistry registry) {
-		registry.observationConfig().observationHandler(new DefaultTracingObservationHandler(otelTracer));
+		registry.observationConfig().observationHandler(new ReactorNettyTracingObservationHandler(otelTracer));
 	}
 }
